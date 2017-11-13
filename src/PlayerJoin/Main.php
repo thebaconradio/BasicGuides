ecd0bd5761c0032053df16dd93dc9403e2aa744f
FreakingDev <31030234+FreakingDev@users.noreply.github.com>
2017-11-13 14:17:15 +0800

    Update Main.php

--- src/PlayerJoin/Main.php
+++ src/PlayerJoin/Main.php
@@ -9,12 +9,12 @@ use pocketmine\event\player\PlayerJoinEvent;
 class Main extends PluginBase implements Listener{
 	
 	public function onEnable(){
-		$this->getServer()->getLogger()->info("JoinMessage enabled!");
+		$this->getServer()->getLogger()->info("PlayerJoin enabled!");
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 	
 	public function onDisable(){
-		$this->getServer()->getLogger()->info("JoinMessage disabled!");
+		$this->getServer()->getLogger()->info("PlayerJoin disabled!");
 	}
 	
 	public function onJoin(PlayerJoinEvent $event){
