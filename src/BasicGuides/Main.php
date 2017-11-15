31ef6f0fd406a41aa07c7aab0343168256aff65d
FreakingDev <31030234+FreakingDev@users.noreply.github.com>
2017-11-15 15:14:25 +0800

    Update Main.php

--- src/BasicGuides/Main.php
+++ src/BasicGuides/Main.php
@@ -6,6 +6,7 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\utils\TextFormat;
+use pocketmine\item\Item;
 
 class Main extends PluginBase implements Listener{
 	
@@ -22,5 +23,11 @@ class Main extends PluginBase implements Listener{
 		$player = $event->getPlayer();
 		$name = $player->getName();
 		$player->sendMessage(TextFormat::GREEN . "Hello " . TextFormat::BLUE . $name);
+		
+		$player->getInventory()->addItem(Item::get(340, 0, 1));
+		
+		$custom = Item::get(340, 0,1);
+		$customName = $custom->setCustomName(TextFormat::GOLD . TextFormat::BOLD . "This is Custom Name");
+		$player->getInventory()->addItem($customName);
 	}
 }
